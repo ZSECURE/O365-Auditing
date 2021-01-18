e712d118a4d2ee274323cfc99302d9cefe78d452
Zak Clifford <46941177+ZSECURE@users.noreply.github.com>
2021-01-18 21:28:09 +0000

    Add files via upload

---
+++ Enable_Audit_Data_Recording.ps1
@@ -0,0 +1,17 @@
+﻿#===============================
+# Enable audit data recording
+#===============================
+
+#==============================
+# Connect to Exchange Online 
+#==============================
+
+# Install the powershell module for Exchange Online
+Install-Module -Name ExchangeOnlineManagement
+
+# This example connects to Exchange Online PowerShell in a Microsoft 365 or Microsoft 365 GCC organization:
+Connect-ExchangeOnline -UserPrincipalName navin@contoso.com -ShowProgress $true
+
+# Verify that audit data recording is enabled by running the following:
+Get-AdminAuditLogConfig | FL UnifiedAuditLogIngestionEnabled
+# Verify that UnifiedAuditLogIngestionEnabled value returned is True
---
+++ Verify_Modern_Authentication_O365.ps1
@@ -0,0 +1,67 @@
+﻿
+
+
+
+
+
+
+
+#===========================
+# Exchange Online 
+#===========================
+
+# Install the powershell module for Exchange Online
+Install-Module -Name ExchangeOnlineManagement
+
+# This example connects to Exchange Online PowerShell in a Microsoft 365 or Microsoft 365 GCC organization:
+Connect-ExchangeOnline -UserPrincipalName navin@contoso.com -ShowProgress $true
+
+# Verify that Modern Authentication is configured by running the following:
+Get-OrganizationConfig | Format-Table -Auto Name,OAuth*
+# Verify that OAuth2ClientProfileEnabled value returned is True
+
+
+
+
+#===========================
+# SharePoint Online 
+#===========================
+
+# Install the powershell module for SharePoint Online
+Install-Module -Name Microsoft.Online.SharePoint.PowerShell
+
+#Connection to SharePoint Online
+$adminUPN="<the full email address of a SharePoint administrator account, example: jdoe@contosotoycompany.onmicrosoft.com>"
+$orgName="<name of your Office 365 organization, example: contosotoycompany>"
+$userCredential = Get-Credential -UserName $adminUPN -Message "Type the password."
+Connect-SPOService -Url https://$orgName-admin.sharepoint.com -Credential $userCredential
+
+# To connect with multifactor authentication (MFA)
+#$orgName="<name of your Office 365 organization, example: contosotoycompany>"
+#Connect-SPOService -Url https://$orgName-admin.sharepoint.com
+
+# Verify that Modern Authentication is configured by running the following: 
+(Get-SPOTenant).LegacyAuthProtocolsEnabled
+# If the returned value is False, then only Modern Authentication is enabled for SharePoint
+
+
+#===========================
+# Skype for Business Online 
+#===========================
+
+# Install the powershell module for Teams
+Install-Module -Name MicrosoftTeams
+
+# Get the credenmtial for the Tenancy
+$credential = Get-Credential
+
+#Connect to Microsoft Teams
+Connect-MicrosoftTeams -Credential $credential
+
+#Connection to Skype for Business Online and import into Ps session
+$session = New-CsOnlineSession -Credential $credential
+Import-PsSession $session
+
+# Verify that Modern Authentication is configured by running the following: 
+(Get-CsOAuthConfiguration).ClientAdalAuthOverride
+# If the returned value is Allowed, then Modern Authentication is configured
